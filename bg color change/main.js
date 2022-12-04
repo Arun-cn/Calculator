f656ac86c8aa0502218e4442887e51dfc0eecf50
arunchandran4321@gmail.com <arunchandran4321@gmail.com>
2022-12-04 14:08:00 +0530

    add input field to change color

--- bg color change/main.js
+++ bg color change/main.js
@@ -1,1 +1,11 @@
-document.getElementsByTagName("button").item(0).addEventListener("click",function(){ document.getElementsByTagName("body").item(0).style.backgroundColor = "Red"; });
+
+
+
+
+document.getElementsByTagName("button").item(0).addEventListener("click",function(){
+  let x = document.getElementsByTagName("input").item(0).value;
+
+document.getElementsByTagName("body").item(0).style.backgroundColor = x;
+
+  
+});
