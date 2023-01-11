c695dfa29211d38a5ecd35a0f16aa5bde6ba0d07
Arun-cn <arunchandran4321@gmail.com>
2023-01-11 11:30:44 +0400

    add onclick function to buttons

---
+++ Calculator/main.js
@@ -0,0 +1,19 @@
+let value =0
+
+function input(vale){
+   let button = document.getElementById("textview").innerHTML;
+    if(vale == 0){
+        button == '0';
+        
+
+
+    }else{
+        button = value + vale;
+    }
+
+   
+}
+
+function eqal (){
+    
+}
